45012de854e2167040dc633d05745133f6d210ec
david ritchey <ritchey0713@gmail.com>
2018-03-06 21:07:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 var katzDeli = [];
 
 function takeANumber(katzDeliLine,name){
-  var spot = katzDeliLine.push(name);
-  spot = katzDeliLine.length;
-  return "Welcome, " + name + "." + "You are " + spot + "in Line"; 
+
 }
