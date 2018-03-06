e3ba66ffe13569335670312dad9c73a660d0a0d0
david ritchey <ritchey0713@gmail.com>
2018-03-06 21:13:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var katzDeli = [];
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
-return `welcome ${name}. You are number ${katzDeliLine.length} in line.`
+return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
   
 }
