ffda502611b3ccce444c9aa4c3b748594306de4b
david ritchey <ritchey0713@gmail.com>
2018-03-06 21:20:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 
 function nowServing(katzDeliLine){
 var name 
-  if (katzDeliLine.length = 0){
+  if (katzDeliLine.length == 0){
   return "There is nobody waiting to be served!"}
   else {
     katzDeliLine.push(name)
