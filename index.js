959413bd8ed4bcad0706477e35b92af3c8711e9e
david ritchey <ritchey0713@gmail.com>
2018-03-06 20:48:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber(katzDeliLine, name){
   var adjustedPostion = katzDeliLine+1;
-  var position = "Welcome, " + name+". You are number" + katzDeliLine +1 +" in line.";
+  var position = "Welcome, " + name+". You are number" + adjustedPostion +" in line.";
   return position;
   
 }
